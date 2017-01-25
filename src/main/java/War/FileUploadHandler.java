ffd57fe00ad5177af7bde05dfce46fa99f72109a
raghavlane <raghavlane@gmail.com>
2017-01-25 12:11:26 +0530

    Update FileUploadHandler.java

--- src/main/java/War/FileUploadHandler.java
+++ src/main/java/War/FileUploadHandler.java
@@ -124,9 +124,12 @@ public class FileUploadHandler extends HttpServlet {
 			CallObject c2 = MainClass.read(out2);
 			CallObject c1 = MainClass.read(out1);
 			request.setAttribute("out1", out1);
+			request.setAttribute("out1done","out1 done");
 			request.setAttribute("out2", out2);
+			request.setAttribute("out2done", "out2done");
 			request.setAttribute("c1", c1.toString());
 			request.setAttribute("c2", c2.toString());
+			request.setAttribute("final", "final done");
 			Set<String> missedset = MainClass.comparecalls(c1, c2);
 			List<String> missedList = new ArrayList<String>();
 			for (String a : missedset) {
